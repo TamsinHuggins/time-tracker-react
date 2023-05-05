7c15231931b6b9ecd049215c41092a1cf12e236c
Tamsin Huggins <tamsin.hns@gmail.com>
2023-05-05 16:27:13 +0100

    Card created. Deck created. Deck populated with 6 cards. Preliminary css added.

---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Card.css";
+
+const Card = (props) => {
+  return (
+    <div className="Card">
+      <div className="Card__stats">
+        <h3>{props.activity}</h3>
+        <h1>{props.time}</h1>
+        <h5>{props.history}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
