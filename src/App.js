9d1d9f14caff77baf878aa37af032fda891a97f5
Tamsin Huggins <tamsin.hns@gmail.com>
2023-05-11 14:13:22 +0100

    added TitleCard element with its own jsx and css files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Card from "./Card";
+import TitleCard from "./TitleCard";
 
 function App() {
   return (
     <div className="App">
       <div className="CardDeck">
+        <TitleCard />
         <Card activity="Work" time={32} history="Last Week - 36hrs" />
         <Card activity="Play" time={10} history="Last Week - 36hrs" />
         <Card activity="Play" time={10} history="Last Week - 36hrs" />
