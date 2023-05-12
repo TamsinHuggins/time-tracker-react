b8410b9448576883e6095c4f3840c4b06cafb81e
Tamsin Huggins <tamsin.hns@gmail.com>
2023-05-12 14:43:26 +0100

    background image SVGs moved to SRC folder. Initial import of image as react component

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Card.css";
+import { ReactComponent as WorkLogo } from "./images/icon-work.svg";
+
+console.log(WorkLogo);
 
 const Card = (props) => {
   return (
     <div style={props.style} className="Card">
+      <WorkLogo className="icon" />
       <div className="Card__stats">
         <h3>{props.activity}</h3>
         <h1>{props.time}</h1>
