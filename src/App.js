1c329d8072f7bec75827c2b951eb2dd042a3914d
Tamsin Huggins <tamsin.hns@gmail.com>
2023-05-12 14:28:28 +0100

    json file moved, imported, and and its data used for some elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Card from "./Card";
 import TitleCard from "./TitleCard";
+import data from "./data.json";
 
 // - Light red (work): hsl(15, 100%, 70%)
 // - Soft blue (play): hsl(195, 74%, 62%)
@@ -17,37 +18,39 @@ function App() {
         <div className="timeCards">
           <Card
             style={{ backgroundColor: "hsl(15, 100%, 70%)" }}
-            activity="Work"
-            time={32}
-            history="Last Week - 36hrs"
+            activity={data[0].title}
+            time={data[0].timeframes.weekly.current}
+            history={
+              "Last Week - " + data[0].timeframes.weekly.previous + "hrs"
+            }
           />
           <Card
             style={{ backgroundColor: "hsl(195, 74%, 62%)" }}
-            activity="Play"
+            activity={data[1].title}
             time={10}
             history="Last Week - 36hrs"
           />
           <Card
             style={{ backgroundColor: "hsl(348, 100%, 68%)" }}
-            activity="Play"
+            activity={data[2].title}
             time={10}
             history="Last Week - 36hrs"
           />
           <Card
             style={{ backgroundColor: "hsl(145, 58%, 55%)" }}
-            activity="Play"
+            activity={data[3].title}
             time={10}
             history="Last Week - 36hrs"
           />
           <Card
             style={{ backgroundColor: "hsl(264, 64%, 52%)" }}
-            activity="Play"
+            activity={data[4].title}
             time={10}
             history="Last Week - 36hrs"
           />
           <Card
             style={{ backgroundColor: "hsl(43, 84%, 65%)" }}
-            activity="Play"
+            activity={data[5].title}
             time={10}
             history="Last Week - 36hrs"
           />
